9498634a91e82cf530bc78945b7f4d58a7b9bbaf
Tetsu Takizawa <tetsu.takizawa5@gmail.com>
2019-08-25 19:00:24 +0900

    Add wait group for safety termination

---
+++ main/main.go
@@ -0,0 +1,99 @@
+package main
+
+import (
+	"context"
+	"github.com/westphae/quaternion"
+	"log"
+	"math"
+	"os"
+	"os/signal"
+	"sync"
+	"syscall"
+	"time"
+)
+
+type Parameters struct {
+	wx, wy, wz, ax, ay, az, mx, my, mz, dt, beta float64
+}
+
+func radianToDegree(rad float64) (deg float64) {
+	deg = rad * 180 / math.Pi
+	return
+}
+
+func main() {
+	defer log.Println("done main")
+
+	// declare signals to treat
+	trapSignals := []os.Signal{
+		syscall.SIGHUP,
+		syscall.SIGINT,
+		syscall.SIGTERM,
+		syscall.SIGQUIT,
+	}
+	// declare a channel to receive signal
+	sigCh := make(chan os.Signal, 1)
+	// receive
+	signal.Notify(sigCh, trapSignals...)
+	//pass the channel to the main processing function
+	ctx, cancel := context.WithCancel(context.Background())
+	// wait a signal in another gotoutine
+	go func() {
+		// block until func receive a signal
+		sig := <-sigCh
+		log.Println("Got signal", sig)
+		cancel()
+	}()
+
+	// signal handler
+	stopCh := make(chan struct{})
+	wg := sync.WaitGroup{}
+
+	q := quaternion.Quaternion{W: 1, X: 0, Y: 0, Z: 0}
+	roll, pitch, yaw := q.Euler()
+	var params = Parameters{
+		wx:   0.01,
+		wy:   0.01,
+		wz:   0.01,
+		ax:   0.01,
+		ay:   0.01,
+		az:   1.00,
+		mx:   30,
+		my:   0,
+		mz:   0,
+		dt:   0.02,
+		beta: 1.0,
+	}
+	// 通信読込 + 接続相手アドレス情報が受取
+	wg.Add(1)
+	go updateParams(&params, stopCh, &wg)
+	wg.Add(1)
+	go sendAttitude(&roll, &pitch, &yaw, stopCh, &wg)
+
+	st := time.Now()
+	i := 1
+
+	for {
+		select {
+		case <-ctx.Done():
+			close(stopCh)
+			break
+		default:
+			params.dt = time.Since(st).Seconds()
+			// update attitude
+			params.updateAttitude(&q)
+			st = time.Now()
+			roll, pitch, yaw = q.Euler()
+
+			roll = radianToDegree(roll)
+			pitch = radianToDegree(pitch)
+			yaw = radianToDegree(yaw)
+
+			log.Println(i, roll, pitch, yaw)
+
+			time.Sleep(10 * time.Millisecond)
+			i++
+		}
+	}
+	wg.Wait()
+}
---
+++ main/socket_client.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"bytes"
+	"encoding/binary"
+	"fmt"
+	"log"
+	"net"
+	"sync"
+	"time"
+)
+
+func float64ToByte(f float64) []byte {
+	var buf bytes.Buffer
+	err := binary.Write(&buf, binary.BigEndian, f)
+	if err != nil {
+		fmt.Println("binary.Write failed:", err)
+	}
+	return buf.Bytes()
+}
+
+func sendAttitude(roll, pitch, yaw *float64, stopCh chan struct{}, wg *sync.WaitGroup) {
+
+	//Done
+	defer func() {wg.Done()}()
+	defer log.Println("done sendAttitude")
+
+	conn, err := net.Dial("udp", "127.0.0.1:60002")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer conn.Close()
+
+	var msg = make([]byte, 0, 24)
+	// メッセージを送信する
+	for {
+		// Stop
+		select {
+		case <- stopCh:
+			log.Println("(goroutine sendAttitude) stop request received")
+			return
+		default:
+			log.Println("(goroutine sendAttitude) runnning")
+			time.Sleep(10 * time.Millisecond)
+			rollB := float64ToByte(*roll)
+			pitchB := float64ToByte(*pitch)
+			yawB := float64ToByte(*yaw)
+			msg = append(msg, rollB...)
+			msg = append(msg, pitchB...)
+			msg = append(msg, yawB...)
+			conn.Write(msg)
+		}
+	}
+}
